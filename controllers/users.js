34a59a0f4f205246e7f91d3aa5c21b4805b82b2d
Burhan Maseel <burhanmaseel@gmail.com>
2019-02-28 19:15:31 +0500

    Files Added

--- controllers/users.js
+++ controllers/users.js
@@ -6,10 +6,14 @@ module.exports = function() {
     return {
         SetRouting: function(router){
             router.get('/', this.indexPage);
+            router.get('/signup', this.getSignup);
         },
 
         indexPage : function (req, res){
             return res.render('index', { test : 'This is a test'});
+        },
+        getSignup : function (req, res){
+            return res.render('signup');
         }
     }
 }
