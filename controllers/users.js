6b123a75a54d023953f0f6ffbf2898f37bb5f0a5
Burhan Maseel <burhanmaseel@users.noreply.github.com>
2019-03-03 23:40:57 +0500

    Revert to 11e1bc7

--- controllers/users.js
+++ controllers/users.js
@@ -6,20 +6,10 @@ module.exports = function() {
     return {
         SetRouting: function(router){
             router.get('/', this.indexPage);
-            router.get('/signup', this.getSignup);
         },
 
         indexPage : function (req, res){
-<<<<<<< HEAD
-            return res.render('index');
-        },
-
-=======
             return res.render('index', { test : 'This is a test'});
-        },
->>>>>>> 34a59a0f4f205246e7f91d3aa5c21b4805b82b2d
-        getSignup : function (req, res){
-            return res.render('signup');
         }
     }
 }
--- server.js
+++ server.js
@@ -2,31 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const http = require('http');
-const cookieParser = require('cookie-parser');
-const validator = require('express-validator');
-const session = require('express-session');
-const MongoStore = require('connect-mongo')(session);
-const mongoose = require('mongoose');
-<<<<<<< HEAD
-const flash = require('flash');
-=======
-const flash = require('connect-flash');
->>>>>>> 34a59a0f4f205246e7f91d3aa5c21b4805b82b2d
-const passport = require('passport');
-
-
 const container = require('./container');
 
 
-
 container.resolve(function(users){
-
-    mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost/fooballkik', {useMongoClient : true});
     
-    mongoose.Promise = global.Promise;
-    mongoose.connect('mongodb://localhost/footballkik', {useMongoClient: true});
-
     const app = SetupExpress();
 
     function SetupExpress(){
@@ -48,35 +28,9 @@ container.resolve(function(users){
 
     function ConfigureExpress(app){
         app.use(express.static('public'));
-        app.use(cookieParser());
         app.set('view engine', 'ejs');
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({extended: true}));
-
-        app.use(validator());
-        app.use(session({
-<<<<<<< HEAD
-            secret : 'thisisasecretkey',
-            resave : true,
-            saveInitialized : true,
-            store : new MongoStore({mongooseConnection: mongoose.connection})
-        }))
-=======
-            secret: 'thisissecretkey',
-            resave: true,
-            saveInitialized: true,
-            store: new MongoStore({mongooseConnection: mongoose.connection})
-        }));
->>>>>>> 34a59a0f4f205246e7f91d3aa5c21b4805b82b2d
-
-        app.use(flash());
-
-        app.use(passport.initialize());
-        app.use(passport.session());
-<<<<<<< HEAD
-
-=======
->>>>>>> 34a59a0f4f205246e7f91d3aa5c21b4805b82b2d
     }
 
 });
