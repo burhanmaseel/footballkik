f4b7d923eeeab3b3e32ab29d08bfc2c5bf33ae8b
Burhan Maseel <burhanmaseel@users.noreply.github.com>
2019-03-03 23:04:15 +0500

    video 20

--- controllers/users.js
+++ controllers/users.js
@@ -6,10 +6,15 @@ module.exports = function() {
     return {
         SetRouting: function(router){
             router.get('/', this.indexPage);
+            router.get('/signup', this.getSignup);
         },
 
         indexPage : function (req, res){
-            return res.render('index', { test : 'This is a test'});
+            return res.render('index');
+        },
+
+        getSignup : function (req, res){
+            return res.render('signup');
         }
     }
 }
--- server.js
+++ server.js
@@ -2,10 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const http = require('http');
+const cookieParser = require('cookie-parser');
+const validator = require('express-validator');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const flash = require('flash');
+const passport = require('passport');
+
+
 const container = require('./container');
 
 
+
 container.resolve(function(users){
+
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost/fooballkik', {useMongoClient : true});
     
     const app = SetupExpress();
 
@@ -28,9 +41,24 @@ container.resolve(function(users){
 
     function ConfigureExpress(app){
         app.use(express.static('public'));
+        app.use(cookieParser());
         app.set('view engine', 'ejs');
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({extended: true}));
+
+        app.use(validator());
+        app.use(session({
+            secret : 'thisisasecretkey',
+            resave : true,
+            saveInitialized : true,
+            store : new MongoStore({mongooseConnection: mongoose.connection})
+        }))
+
+        app.use(flash());
+
+        app.use(passport.initialize());
+        app.use(passport.session());
+
     }
 
 });
