0f8eea48dfc1566c263f0e8ecd32ff8a2ad7eb4a
Burhan Maseel <burhanmaseel@users.noreply.github.com>
2019-03-04 23:52:53 +0500

    commit signup form

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const http = require('http');
+const cookieParser = require('cookie-parser');
+const validator = require('express-validator');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
+const flash = require('connect-flash');
+
+
+
 const container = require('./container');
 
 
 container.resolve(function(users){
     
+    mongoose.Promise = global.Promise;
+    mongoose.connect('mongodb://localhost/footballkik');
+
     const app = SetupExpress();
 
     function SetupExpress(){
@@ -27,10 +39,23 @@ container.resolve(function(users){
     
 
     function ConfigureExpress(app){
+        require('./passport/passport-local');
+
         app.use(express.static('public'));
+        app.use(cookieParser());
         app.set('view engine', 'ejs');
         app.use(bodyParser.json());
         app.use(bodyParser.urlencoded({extended: true}));
+
+        app.use(validator());
+        app.use(session({
+            secret : 'thisisasecretkey',
+            resave : true,
+            saveUninitialized : true,
+            store : new MongoStore({ mongooseConnection : mongoose.connection})
+        }));
+
+        app.use(flash());
     }
 
 });
